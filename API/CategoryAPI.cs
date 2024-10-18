8ac49393f63b8150752a5fc80534f336a79e2326
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-10-17 20:38:20 -0500

    commiting the changes made to endpoints

--- API/CategoryAPI.cs
+++ API/CategoryAPI.cs
@@ -1,10 +1,22 @@
-﻿namespace FanFusion_BE.API
+﻿using FanFusion_BE.DTO;
+
+namespace FanFusion_BE.API
 {
     public class CategoryAPI
     {
         public static void Map(WebApplication app)
         {
+            // get all categories
+            app.MapGet("/categories", (FanFusionDbContext db) =>
+            {
+                var allCategories = db.Categories.ToList();
 
+                if (!allCategories.Any())
+                {
+                    return Results.Ok("There are no aviliable categories to display");
+                }
+                return Results.Ok(allCategories);
+            });
 
         }
     }
