8c9896ac399d08d1fef342795931dc0e02ea10c5
Fletcher Moore <fletcherjmoore14@gmail.com>
2024-10-28 20:35:49 -0500

    Commiting changes to the get Single Story call

--- DTO/UserDto.cs
+++ DTO/UserDto.cs
@@ -7,11 +7,15 @@ namespace FanFusion_BE.DTO
     {
         public int Id { get; set; }
         public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Uid { get; set; }
         public string Image {  get; set; }
         public UserDto(User user)
         {
             Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
             Username = user.Username;
             Uid = user.Uid;
             Image = user.Image;
